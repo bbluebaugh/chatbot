78347f4b4b9e43197aec99409df595a0e29cc8f2
Brian <bbluebaugh@yahoo.com>
2020-05-29 10:27:23 -0400

    Author: BB Date: 05/29/2020
    Updated Initial Code
    Added more of the base logic for the program.

--- chatbot.py
+++ chatbot.py
@@ -13,16 +13,43 @@ with open("intents.json") as file:
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
 
+words = [stemmer.stem(w.lower()) for w in words if w not in "?"] #convert words to lowercase to prevent confusion with capitals
+words = sorted(List(set(words)))
 
-        
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+training = numpy.array(training)
+output = numpy.array(output)
